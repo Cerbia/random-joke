ee48d08f3032102c2d9568cc6c8a32947f8f645e
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-02 22:38:32 +0200

    Add quote functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
+// 1. max. 140 znaków
+// Cytat powinien pobierać się przy pierwszym załadowaniu strony oraz 
+// przy kliknięciu w przycisk z napisem 'Random quote'.
+// Przycisk "Tweet" powinien rzecz jasna udostępnić tweeta z cytatem.
+
+var prefix = "https://cryptic-headland-94862.herokuapp.com/";
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+var trigger = document.querySelector('.trigger');
+trigger.addEventListener('click', function() {
+	getQuote();
+
+});
+
+function getQuote() {
+	fetch(prefix + quoteUrl, { cache: "no-store" })
+  		.then(function(response) {
+    		return response.json();
+  		})
+  		.then(function(myJson) {
+    		console.log(myJson);
+    		return myJson;
+  		})
+  		.then(createTweet);
+}
+
+function createTweet(input) {
+	
+	//var data = input[0];
+	console.log('data from callback' + input)
+}
+
+//-------------------------------------------
+
 var url = 'http://api.icndb.com/jokes/random';
 
 var joke = document.getElementById('joke');

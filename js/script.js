ed62001d112709675e4d7bf5ee1c7427c56c3023
Bernadetta Wąsowska <bernadetta.wasowska@gmail.com>
2018-07-09 09:51:20 +0200

    Finish quote feature

--- js/script.js
+++ js/script.js
@@ -1,62 +1,75 @@
-// 1. max. 140 znaków
-// Cytat powinien pobierać się przy pierwszym załadowaniu strony oraz 
-// przy kliknięciu w przycisk z napisem 'Random quote'.
-// Przycisk "Tweet" powinien rzecz jasna udostępnić tweeta z cytatem.
+const prefix = "https://cryptic-headland-94862.herokuapp.com/";
 
-var prefix = "https://cryptic-headland-94862.herokuapp.com/";
+const tweetLink = "https://twitter.com/intent/tweet?text=";
+const quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+const quoteElement = document.querySelector('.quote');
+const authorElement = document.querySelector('.author');
 
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
-var trigger = document.querySelector('.trigger');
+const trigger = document.querySelector('.trigger');
 trigger.addEventListener('click', function() {
-	getQuote();
+    getQuote();
 
 });
 
 function getQuote() {
-	fetch(prefix + quoteUrl, { cache: "no-store" })
-  		.then(function(response) {
-    		return response.json();
-  		})
-  		.then(function(myJson) {
-    		console.log(myJson);
-    		return myJson;
-  		})
-  		.then(createTweet);
+    return fetch(prefix + quoteUrl, { cache: "no-store" })
+        .then((response) => response.json())
+        .then(createTweet)
+        .catch(error => console.warn(error));
 }
 
-function createTweet(input) {
-	
-	//var data = input[0];
-	console.log('data from callback' + input)
+function createTweet(responseData) {
+
+    dataElement= document.createElement('div');
+    dataElement.innerHTML = responseData[0].content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = responseData[0].title;
+    if(!quoteAuthor.length) {
+        quoteAuthor = "Author unknown"
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if(tweetText.length > 140){
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    } 
 }
 
-//-------------------------------------------
+window.onload = function() {
+    getQuote();
+};
+
+/*
 
 var url = 'http://api.icndb.com/jokes/random';
 
 var joke = document.getElementById('joke');
 var button = document.getElementById('get-joke');
 button.addEventListener('click', function() {
-	getRequest();
+    getRequest();
 })
 
 function getRequest() {
-	var request = new XMLHttpRequest();
-	request.open('GET', 'http://api.icndb.com/jokes/random'); // just like it's been until now
-	request.onload = function() {
-		if (request.status == 200) {
-			console.log(request.response);
-			obj = JSON.parse(request.response);
-			console.log(obj);
-			joke.innerHTML = obj.value.joke;
-
-		}
-	};
-	request.send();
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://api.icndb.com/jokes/random'); // just like it's been until now
+    request.onload = function() {
+        if (request.status == 200) {
+            //console.log(request.response);
+            obj = JSON.parse(request.response);
+            //console.log(obj);
+            joke.innerHTML = obj.value.joke;
+        }
+    };
+    request.send();
 }
 
 window.onload = function() {
-  	getRequest();
+    getRequest();
+    getQuote();
 };
+*/
